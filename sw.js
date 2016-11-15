66eaee0ea9339635249033ca2e2ac972bf163fda
Gilberto Tavares <camaleaun@outlook.com>
2016-11-15 02:09:02 -0200

    Test v2

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 
-var version = 'v1';
-var staticCacheName = 'bizual-static-v1';
+require('serviceworker-cache-polyfill');
+
+var version = 'v2';
+var staticCacheName = 'bizual-static-v2';
 
 self.oninstall = function(event) {
   self.skipWaiting();
@@ -19,6 +21,38 @@ self.oninstall = function(event) {
         'js/aceual.js',
         'imgs/icon.png',
       ]);
+    })
+  );
+};
+
+var expectedCaches = [
+  staticCacheName
+];
+
+self.onactivate = function(event) {
+  if (self.clients && clients.claim) {
+    clients.claim();
+  }
+
+  // remove caches beginning "trains-" that aren't in
+  // expectedCaches
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+};
+
+self.onfetch = function(event) {
+  var requestURL = new URL(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request, {
+      ignoreVary: true
     })
   );
 };
