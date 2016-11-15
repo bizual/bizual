01d233f8baa2f2816a2100181a6914252cdfdf17
Gilberto Tavares <camaleaun@outlook.com>
2016-11-15 03:07:46 -0200

    Test as minimal

--- sw.js
+++ sw.js
@@ -1,24 +1,8 @@
 
-//'./',
-//'css/all.css',
-//'css/fonts/MaterialIcons-Regular.eot',
-//'css/fonts/MaterialIcons-Regular.woff',
-//'css/fonts/MaterialIcons-Regular.woff2',
-//'css/fonts/MaterialIcons-Regular.ttf',
-//'js/page.js',
-//'js/material.min.js',
-//'js/aceual.js',
-//'imgs/icon.png'
-
-'use strict';
-
-const PRECACHE = 'bizual-static-v2';
-const RUNTIME = 'runtime';
-
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll([
+    caches.create('bizual-static-v6').then(function(cache) {
+      return cache.add({
         './',
         'css/all.css',
         'css/fonts/MaterialIcons-Regular.eot',
@@ -29,46 +13,12 @@ self.addEventListener('install', event => {
         'js/material.min.js',
         'js/aceual.js',
         'imgs/icon.png'
-      ]))
-      .then(self.skipWaiting())
+      });
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+self.addEventListener('fetch', function(event) {
+  event.respondeWith(
+    caches.match(event.request);
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
 });
