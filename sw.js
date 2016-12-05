900d62de5f246b021058636c6090af2442860527
Gilberto Tavares <camaleaun@outlook.com>
2016-12-05 18:14:33 -0200

    Remove cache

---
+++ sw.js
@@ -0,0 +1,34 @@
+/*global self,caches*/
+var version = 'v14',
+    cacheName = 'bizual-static-' + version,
+    cacheFiles = [
+    './'
+];
+
+self.oninstall = function (event) {
+  'use strict';
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+};
+
+self.onfetch = function (event) {
+  'use strict';
+  event.respondWith(
+    caches.match(event.request)
+  );
+};
+
+self.onactivate = function activator (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+};
