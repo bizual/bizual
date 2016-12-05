470e0336c3536a6ca5b77313274208104eeccfbb
Gilberto Tavares <camaleaun@outlook.com>
2016-12-05 18:23:43 -0200

    Attempt unregister

---
+++ sw.js
@@ -0,0 +1,31 @@
+/*global self,caches*/
+var version = 'v15',
+    cacheName = 'bizual-static-' + version,
+    cacheFiles = [
+    './'
+];
+self.oninstall = function (event) {
+  'use strict';
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+};
+self.onfetch = function (event) {
+  'use strict';
+  event.respondWith(
+    caches.match(event.request)
+  );
+};
+self.onactivate = function activator (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+};
